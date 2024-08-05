31dced7c1f0613c7821a7068c8ca001ad1e8fc2c
RaniaDg12 <rania.doggaz@etudiant-enit.utm.tn>
2024-08-05 13:49:33 +0100

    Make necessary config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthComponent } from "./components/auth/auth.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HomeComponent } from './components/home/home.component';
+import { SitesComponent } from './components/sites/sites.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { UsersComponent } from './components/users/users.component';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    AuthComponent,
+    NavbarComponent,
+    SidebarComponent, 
+    DashboardComponent,
+    HomeComponent,
+    SitesComponent,
+    ArticlesComponent,
+    UsersComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Dashboard';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,21 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { SitesComponent } from './components/sites/sites.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { UsersComponent } from './components/users/users.component';
+
+
 
 export const routes: Routes = [
     { path: '', component: AuthComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'navbar', component: NavbarComponent },
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'sites', component: SitesComponent},
+    { path: 'articles', component: ArticlesComponent},
+    { path: 'users', component: UsersComponent},
+
 ];
